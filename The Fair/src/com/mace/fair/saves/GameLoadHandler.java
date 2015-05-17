341e3600f071fedac30aec3a927c1326f28eabb2
M. Walker <masimba@blueyonder.co.uk>
2015-05-17 14:34:40 +0100

    Writing game loading file

--- The Fair/src/com/mace/fair/saves/GameLoadHandler.java
+++ The Fair/src/com/mace/fair/saves/GameLoadHandler.java
@@ -1,5 +1,31 @@
 package com.mace.fair.saves;
 
-public class GameLoadHandler {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import com.mace.fair.entities.Player;
+import com.mace.fair.entities.Zombie;
+import com.mace.fair.map.Map;
+import com.mace.fair.pills.Pill;
+
+public class GameLoadHandler {
+	/***********************************************************
+	 * Will handle loading all the games data from a save file.*
+	 ***********************************************************/
+	private Scanner readSaveData;
+	private Player player;
+	private Map map;
+	private ArrayList<Zombie> zombies;
+	private ArrayList<Pill> pills;
+
+	public GameLoadHandler(Player player, Map map, ArrayList<Zombie> zombies, ArrayList<Pill> pills) {
+		this.player = player;
+		this.map = map;
+		this.zombies = zombies;
+		this.pills = pills;
+	}
+
+	public void loadGame() {
+
+	}
 }
